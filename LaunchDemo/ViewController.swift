ecbbe7c4ceedaae06ffcda5d3fc97b017f6c2027
wzdg-whl <wzdg-whl@wenhelumacbook-air.local>
2016-11-29 11:50:01 +0800

    Initial Commit

---
+++ LaunchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchDemo
+//
+//  Created by wzdg-whl on 16/11/29.
+//  Copyright © 2016年 WHL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
